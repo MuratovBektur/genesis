4fdd99e028f6c1a40685c31036ea268d3f27bffa
Bektur <prostreyd@gmail.com>
2023-02-17 15:04:33 +0600

    fix: removed passing unused header in nginx conf, fixed passing NODE_ENV variables and disabled swagger in prod mode

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,20 +2,24 @@ import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
+const isDev = process.env.NODE_ENV === 'dev';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const globalPrefix = 'api';
 
+  if (isDev) {
+    const config = new DocumentBuilder()
+      .setTitle('Your title')
+      .setDescription('Your description')
+      .setVersion('1.0')
+      .addTag('Your tag')
+      .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup(`${globalPrefix}/explorer`, app, document);
+  }
   app.setGlobalPrefix(globalPrefix);
-  const config = new DocumentBuilder()
-    .setTitle('Your title')
-    .setDescription('Your description')
-    .setVersion('1.0')
-    .addTag('Your tag')
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup(`${globalPrefix}/explorer`, app, document);
 
   await app.listen(5000);
 }
